def7e18234a92053257c7e68e2aca3399960ba4d
Matt Weber <1062734+mweberxyz@users.noreply.github.com>
2021-04-19 16:55:57 -0400

    avoid leaking webhook url into cloudformation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Construct, CustomResource } from "@aws-cdk/core";
 import { IUser, CfnAccessKey } from "@aws-cdk/aws-iam";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as path from "path";
+import * as crypto from "crypto";
 
 import type { AccessKeyViaSlackProperties } from "./types";
 
@@ -11,11 +12,18 @@ const pkgVersion = require("../package.json").version;
 export default class AccessKeyViaSlack extends Construct {
   constructor(scope: Construct, id: string, props: { user: IUser; slackWebhookUrl: string }) {
     super(scope, id);
+    const singletonPostfix = crypto
+      .createHash("sha256")
+      .update(props.slackWebhookUrl)
+      .update(pkgVersion)
+      .digest("hex")
+      .substring(0, 16);
+
     const fn = new lambda.SingletonFunction(this, "AccessKeyViaSlackFunction", {
       runtime: lambda.Runtime.NODEJS_14_X,
       handler: "index.handler",
       code: lambda.Code.fromAsset(path.join(__dirname, "./lambda")),
-      uuid: `access-key-via-slack-${pkgVersion}-${props.slackWebhookUrl}`,
+      uuid: `Singleton${singletonPostfix}`,
       environment: {
         WEBHOOK_URL: props.slackWebhookUrl,
       },
